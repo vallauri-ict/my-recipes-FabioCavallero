6decc792a9e19fd73c8d473ddd1cf6ca43525e18
FabioCavallero <f.cavallero.1286@vallauri.edu>
2021-11-18 11:23:02 +0100

    Recipe item now correctly connected to recipe list

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { RecipeModel } from 'src/app/models/recipe.model';
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-
+  @Input() recipe:RecipeModel;
   constructor() { }
-
   ngOnInit(): void {
   }
-
 }
