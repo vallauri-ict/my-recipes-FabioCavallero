07e488135af43fdb62b5ad5c49ee77094a440a0b
FabioCavallero <f.cavallero.1286@vallauri.edu>
2021-12-16 19:32:18 +0100

    Correction of some errors

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RecipeService } from 'src/app/shared/recipe.service';
+
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  constructor(recipeService:RecipeService) { }
-  ngOnInit(): void {}
+
+  constructor(public recipeService:RecipeService) { }
+
+  ngOnInit(): void {
+  }
+
 }
